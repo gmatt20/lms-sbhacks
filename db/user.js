210c4d31c069879554c8c70344ddd45f351b6fe6
Christopher-Chhim <christopher.chhim123@gmail.com>
2026-01-10 16:11:15 -0800

    created user schema

---
+++ db/user.js
@@ -0,0 +1,8 @@
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    role: { type: String, enum: ['professor', 'student'], required: true },
+    school: String,
+    createdAt: { type: Date, default: Date.now}
+});
