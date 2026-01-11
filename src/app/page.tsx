66e9e75d39026a82f17ae5b5dab362d4cb73cc56
Matthew Garcia <gmatthew2765@gmail.com>
2026-01-10 16:09:08 -0800

    added pdf component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import { Body } from "@/components/body/Body";
+import Pdf from "@/components/uploader/pdf";
+import { useAuth } from "@clerk/nextjs";
 
 export default function Home() {
+  const { isSignedIn } = useAuth();
+
   return (
     <div>
       <div className="mb-5 text-3xl bold">Next.js Voice Agent</div>
-      <Body />
+      {isSignedIn && <Body />}
+      <Pdf />
     </div>
   );
 }
